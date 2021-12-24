6e9002bc6f47d3d1d89db0c2cd9f91755adf1e48
Munna Aziz <83208376+munnaaziz-wp@users.noreply.github.com>
2021-12-24 12:44:07 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+import {
+  SNAKE_SPEED,
+  updateGame as updateSnake,
+  drawElement as drawSnake,
+  getSnakeHead,
+  snakeIntersection,
+} from "./snake.js";
+import { updateGame as updateFood, drawElement as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
+
+const gameBoard = document.getElementById("game-board");
+
+let lastRenderTime = 0;
+let gameOver = false;
+
+function main(currentTime) {
+  if (gameOver) {
+    if (confirm("You lose, Press OK to restart.")) {
+      window.location = "/";
+    }
+    return;
+  }
+
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  lastRenderTime = currentTime;
+
+  updateGame();
+  drawElement();
+}
+
+window.requestAnimationFrame(main);
+
+function updateGame() {
+  updateSnake();
+  updateFood();
+  checkDeath();
+}
+
+function drawElement() {
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
